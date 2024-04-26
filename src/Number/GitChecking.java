1b10b3b0ef0563ef084bdd9964e0e507d41e701f
Vivek Kumar <vivekkumar706277@gmail.com>
2024-04-26 15:57:19 +0530

    Git Checking Java class added

---
+++ src/Number/GitChecking.java
@@ -0,0 +1,16 @@
+package Number;
+
+public class GitChecking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bittu Vivek");
+		
+		int x = 69;
+		
+		System.out.println(x);
+
+	}
+
+}
